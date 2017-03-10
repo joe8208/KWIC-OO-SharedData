947617fa452e64a95613953f61a59cad29b153cd
Joseph Harzheim <josephh@harzheimsoftware.com>
2017-03-10 16:31:02 -0600

    Add project files.

---
+++ KWIC-OO-SharedData/Input.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWIC_OO_SharedData
+{
+    public class Input
+    {
+        public List<string> Execute()
+        {
+            bool isValidFile = false;
+
+            Console.Clear();
+            Console.Title = "KWIC - OO and Shared Data";
+            string filePath = "";
+            List<string> inputFile = new List<string>();
+
+            while (!isValidFile)
+            {
+                Console.WriteLine("Please enter a valid path of data file:");
+                filePath = Console.ReadLine();
+
+                try
+                {
+                    inputFile = File.ReadLines(filePath).ToList();
+                    isValidFile = true;
+                }
+                catch (Exception)
+                {
+                    isValidFile = false;
+                }
+            }
+                        
+            return inputFile;
+        }
+    }
+}
---
+++ KWIC-OO-SharedData/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWIC_OO_SharedData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
