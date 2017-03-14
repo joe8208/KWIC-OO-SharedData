0a6f00c54d8d336dadb655fff0478bead5cc5d20
Joseph Harzheim <josephh@harzheimsoftware.com>
2017-03-13 20:39:34 -0500

    Added Custom Sorter.

--- KWIC-OO-SharedData/Alphabetizer.cs
+++ KWIC-OO-SharedData/Alphabetizer.cs
@@ -117,7 +117,9 @@ namespace KWIC_OO_SharedData
                     }
 
                     // comparer
-                    int compared = shift.CompareTo(midLine);
+                    //int compared = shift.CompareTo(midLine);
+                    int compared = CustomComparable(shift, midLine);
+
 
                     if (compared > 0)
                         low = mid + 1;
@@ -138,6 +140,58 @@ namespace KWIC_OO_SharedData
                     alphabetizedCount++;
 
             }
+        }
+
+        int CustomComparable(string x, string y)
+        {
+            // make sorter sort lower case characters first then uppercase characters
+            // take line conver it into a array of charaters and compare to the second string if it is less or greater on the sort order.
+            int maxStringLength = 0;
+
+            if (x.Length <= y.Length)
+                maxStringLength = x.Length;
+            else
+            {
+                maxStringLength = y.Length;
+            }
+
+            for (int i = 0; i < maxStringLength; i++)
+            {
+                char a = x[i];
+                char b = y[i];
+
+                if (a.ToString().ToLower() == b.ToString().ToLower())
+                {
+                    if (a == b)
+                    {
+                        if (i + 1 == maxStringLength)
+                        {
+                            if (x.Length < y.Length)
+                                return -1;
+                            else
+                                return 1;
+                        }
+                    }
+                    else
+                    {
+
+                        if (a.ToString().ToLower() == b.ToString())
+                        {
+                            return 1;
+                        }
+                        else if (a.ToString().ToLower() != b.ToString())
+                        {
+                            return -1;
+                        }
+                    }
+
+                }
+                else
+                {
+                    return string.Compare(a.ToString(), b.ToString());
+                }
+            }
+            return 0;
         }
         
     }
